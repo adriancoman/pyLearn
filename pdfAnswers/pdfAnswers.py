84bb1a13a4711f6e99b7ff670af792683ec0c1fb
adrianc <contact@adriancoman.ro>
2024-02-13 08:14:46 +0200

    - created a different directory for the experiments - added a pdf Q&A sample

---
+++ pdfAnswers/pdfAnswers.py
@@ -0,0 +1,22 @@
+from transformers import pipeline
+from PyPDF2 import PdfReader
+
+
+def get_text_from_pdf():
+    reader = PdfReader('pdfAnswers/sample.pdf')
+    final_text = ''
+    for page in reader.pages:
+        final_text += page.extract_text()
+    return final_text
+
+
+def answer_question(context, question):
+    pipe = pipeline("question-answering", model="deepset/roberta-base-squad2")
+    response = pipe(question=question, context=context)["answer"]
+    return response
+
+
+pdf_text = get_text_from_pdf()
+question_to_use = "where can i find more examples of how to use HTML and CSS?"
+answer = answer_question(pdf_text, question_to_use)
+print(answer)
